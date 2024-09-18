de728ad99b1f6fc4f87fdc49ba300cab008036bc
Wojciech Turek <wojciechturek9@gmail.com>
2024-09-18 22:58:08 +0200

    7 commit

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,4 +1,3 @@
-import 'package:education_app/core/app/providers/user_provider.dart';
 import 'package:education_app/core/common/views/page_under_construction.dart';
 import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:education_app/core/services/injection_container.dart';
@@ -11,79 +10,10 @@ import 'package:education_app/src/on_boarding/data/datasources/on_boarding_local
 import 'package:education_app/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
 import 'package:education_app/src/on_boarding/presentation/views/on_boarding_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart' as fui;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Route<dynamic> generateRoute(RouteSettings settings) {
-  switch (settings.name) {
-    case '/':
-      final prefs = sl<SharedPreferences>();
-      return _pageBuilder(
-        (context) {
-          if (prefs.getBool(kFirstTimerKey) ?? true) {
-            return BlocProvider(
-              create: (_) => sl<OnBoardingCubit>(),
-              child: const OnBoardingScreen(),
-            );
-          } else if (sl<FirebaseAuth>().currentUser != null) {
-            final user = sl<FirebaseAuth>().currentUser!;
-            final localUser = LocalUserModel(
-              uid: user.uid,
-              email: user.email ?? '',
-              points: 0,
-              fullName: user.displayName ?? '',
-            );
-            context.userProvider.initUser(localUser);
-            return const Dashboard();
-          }
-          return BlocProvider(
-            create: (_) => sl<AuthBloc>(),
-            child: const SignInScreen(),
-          );
-        },
-        settings: settings,
-      );
-    case SignInScreen.routeName:
-      return _pageBuilder(
-        (_) => BlocProvider(
-          create: (_) => sl<AuthBloc>(),
-          child: const SignInScreen(),
-        ),
-        settings: settings,
-      );
-    case SignUpScreen.routeName:
-      return _pageBuilder(
-        (_) => BlocProvider(
-          create: (_) => sl<AuthBloc>(),
-          child: const SignUpScreen(),
-        ),
-        settings: settings,
-      );
-    case Dashboard.routeName:
-      return _pageBuilder(
-        (_) => const Dashboard(),
-        settings: settings,
-      );
-    default:
-      return _pageBuilder(
-        (_) => const PageUnderConstruction(),
-        settings: settings,
-      );
-  }
-}
-
-PageRouteBuilder<dynamic> _pageBuilder(
-  Widget Function(BuildContext) page, {
-  required RouteSettings settings,
-}) {
-  return PageRouteBuilder(
-    settings: settings,
-    transitionsBuilder: (_, animation, __, child) => FadeTransition(
-      opacity: animation,
-      child: child,
-    ),
-    pageBuilder: (context, _, __) => page(context),
-  );
-}
+part 'router.main.dart';
--- lib/src/auth/presentation/views/sign_in_screen.dart
+++ lib/src/auth/presentation/views/sign_in_screen.dart
@@ -1,10 +1,14 @@
 import 'package:education_app/core/app/providers/user_provider.dart';
 import 'package:education_app/core/common/widgets/gradient_background.dart';
+import 'package:education_app/core/common/widgets/rounded_button.dart';
+import 'package:education_app/core/res/fonts.dart';
 import 'package:education_app/core/res/media_res.dart';
 import 'package:education_app/core/utils/core_utils.dart';
 import 'package:education_app/src/auth/data/models/user_model.dart';
 import 'package:education_app/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:education_app/src/auth/presentation/widgets/sign_in_form.dart';
 import 'package:education_app/src/dashboard/presentation/views/dashboard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -46,8 +50,86 @@ class _SignInScreenState extends State<SignInScreen> {
           return GradientBackground(
             image: MediaRes.authGradientBackground,
             child: SafeArea(
-              child: ListView(
-                shrinkWrap: true,
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  children: [
+                    const Text(
+                      'Easy to learn, discover more skills.',
+                      style: TextStyle(
+                        fontFamily: Fonts.aeonik,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Sign in to your account',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Baseline(
+                          baseline: 100,
+                          baselineType: TextBaseline.alphabetic,
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(
+                                context,
+                                SignInScreen.routeName,
+                              );
+                            },
+                            child: const Text(
+                              'Register account?',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        SignInForm(
+                          emailController: emailController,
+                          paswordController: passwordController,
+                          formKey: formKey,
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/forgot-password');
+                            },
+                            child: const Text('Forgot password?'),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        if (state is AuthLoading)
+                          const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        else
+                          RoundedButton(
+                            label: 'Sign In',
+                            onPressed: () {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                              FirebaseAuth.instance.currentUser?.reload();
+                              if (formKey.currentState!.validate()) {
+                                context.read<AuthBloc>().add(
+                                      SignInEvent(
+                                        email: emailController.text.trim(),
+                                        password:
+                                            passwordController.text.trim(),
+                                      ),
+                                    );
+                              }
+                            },
+                          ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           );
