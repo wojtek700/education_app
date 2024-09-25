fcef2b56abdf4c63c498aab6c69796fe1be14c5d
Wojciech Turek <wojciechturek9@gmail.com>
2024-09-25 23:44:08 +0200

    14 commit

---
+++ lib/core/common/features/course/domain/repos/course_repo.dart
@@ -0,0 +1,9 @@
+import 'package:education_app/core/common/features/course/domain/entities/course.dart';
+import 'package:education_app/core/utils/typdefs.dart';
+
+abstract class CourseRepo {
+  const CourseRepo();
+
+  ResultFuture<List<Course>> getCourses();
+  ResultFuture<void> addCourse(Course course);
+}
--- lib/core/common/widgets/nested_back_button.dart
+++ lib/core/common/widgets/nested_back_button.dart
@@ -1,3 +1,4 @@
+import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
 
 class NestedBackButton extends StatelessWidget {
@@ -5,11 +6,27 @@ class NestedBackButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {},
-      icon: Theme.of(context).platform == TargetPlatform.iOS
-          ? const Icon(Icons.arrow_back_ios_new)
-          : const Icon(Icons.arrow_back),
+    return WillPopScope(
+      onWillPop: () async {
+        try {
+          context.pop();
+          return false;
+        } catch (_) {
+          return true;
+        }
+      },
+      child: IconButton(
+        onPressed: () {
+          try {
+            context.pop();
+          } catch (_) {
+            Navigator.of(context).pop();
+          }
+        },
+        icon: Theme.of(context).platform == TargetPlatform.iOS
+            ? const Icon(Icons.arrow_back_ios_new)
+            : const Icon(Icons.arrow_back),
+      ),
     );
   }
 }
