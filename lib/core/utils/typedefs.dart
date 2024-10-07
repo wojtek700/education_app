739ab37ba306fd5a3755ae3a1bf547b7e3687f46
Wojciech Turek <wojciechturek9@gmail.com>
2024-10-07 23:34:40 +0200

    26 commit

--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -3,4 +3,6 @@ import 'package:education_app/core/errors/failures.dart';
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 
+typedef ResultStream<T> = Stream<Either<Failure, T>>;
+
 typedef DataMap = Map<String, dynamic>;
