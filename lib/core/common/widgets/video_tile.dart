50cd979774541c9203858ac5b2379c0627a536bc
Wojciech Turek <wojciechturek9@gmail.com>
2024-10-13 23:39:53 +0200

    31 commit

---
+++ lib/core/common/widgets/video_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VideoTile extends StatelessWidget {
+  const VideoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
